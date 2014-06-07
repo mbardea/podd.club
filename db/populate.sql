17f75d8102ecb5bc882ac9e4b522a7a5e005a5a1
Manuel Bardea <mbardea@gmail.com>
2014-06-06 20:48:23 -0400

    Initial Commit

---
+++ db/populate.sql
@@ -0,0 +1,5 @@
+insert into categories(id, name)
+    values(1, 'Databases');
+
+insert into podcasts(category_id, title, description, location)
+    values(1, 'Using Mongodb', 'Mongo DB presentation', 'podcasts/mongo/mongo.mp3');
