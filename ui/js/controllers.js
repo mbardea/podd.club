4744b7154e1a0b637ae6eb13bf8b6763dc0f3437
pbardea <pbardea@gmail.com>
2014-07-06 21:29:19 -0400

    Fixed podcast count per category

--- ui/js/controllers.js
+++ ui/js/controllers.js
@@ -6,21 +6,15 @@ poddclubApp.controller('CategoryListCtrl', function ($scope) {
 
   $scope.categories = [
     {'name': 'Tech',
-     'snippet': 'Tech talks.',
-     'podcastCount': 13,
      'podcasts':[{'name':'TWIT','episodeNumber':432,'author':'Leo Laport','length':'900s','url':'http://www.youtube.com/blahblah'},{'name':'Tech Talk','episodeNumber':432,'author':'Leo Laport','length':'900s','url':'http://www.youtube.com/blahblah'}] },
     {'name': 'TED Talks',
-     'snippet': 'Ideas worth spreading.',
-     'podcastCount': 45,
      'podcasts':[{'name':'TED Talks 1','episodeNumber':432,'author':'Leo Laport','length':'900s','url':'http://www.youtube.com/blahblah'},{'name':'TED Talks 2','episodeNumber':432,'author':'Leo Laport','length':'900s','url':'http://www.youtube.com/blahblah'}] },
     {'name': 'Astrophysics',
-     'snippet': 'Podcasts from the stars.',
-     'podcastCount': 6,
      'podcasts':[{'name':'Astro1','episodeNumber':42,'author':'Neil deGrasse Tyson','length':'800s','url':'http://www.youtube.com/blahblah'},{'name':'Astro2','episodeNumber':34,'author':'Neil deGrasse Tyson','length':'700s','url':'http://www.youtube.com/blahblah'}] },
   ];
   
   $scope.addCategory = function(){
-	  $scope.categories.push({'name':$scope.newCategory,'snippet':"hi","podcastCount":0})
+	  $scope.categories.push({'name':$scope.newCategory})
 	  $scope.newCategory = ''
   }
 
@@ -34,14 +28,6 @@ poddclubApp.controller('CategoryListCtrl', function ($scope) {
       }
     }
   }
-
-  // $scope.addPodcast = function(){
-  //   for (var category in $scope.categories){
-  //     var podcasts = category['podcasts']
-  //     podcasts.push({'name':'new','episodeNumber':333,'author':'whoKnows','lenght':'90s','url':$scope.newPodcastURL}
-  //     $scope.newPodcastUrl = ''
-  //   }
-  // }
 
   $scope.changeCurrentCategory = function(name){
     $scope.currentCategory = name
